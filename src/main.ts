600e597dcac2d7ffad383dcc71c51e6afefa2ef6
Edar José Blanco Rodríguez <edar.blanco@gmail.com>
2026-02-05 05:32:47 -0500

    Fix deploy

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // CORS configuration
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
     credentials: true,
   });
 
